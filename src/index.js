639199161249fdecad0f478c4023c3221f07165d
Gaston <gastonamarilla2013@gmail.com>
2020-05-19 12:00:35 -0300

    Empezando las views

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express ();
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine','.hbs');
+//Midllewares
+
+//Global variables
+
+//Routes
+
+//Static Files
+
+//Server is listenning
+
+app.listen(app.get('port'), () =>{
+    console.log('Server on port ', app.get('port'));
+});
